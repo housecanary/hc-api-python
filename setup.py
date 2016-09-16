ee8355e8c3d06c6add56ce6962d5ccb9432c98c5
Jeff Francisco <jeff.francisco7@gmail.com>
2016-09-16 14:52:30 -0400

    Add missing package name and bump version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(name='housecanary',
-      version='0.5',
+      version='0.5.1',
       description='Client Wrapper for the HouseCanary API',
       long_description=readme(),
       url='http://github.com/housecanary/hc-api-python',
       author='HouseCanary',
       author_email='techops@housecanary.com',
       license='MIT',
-      packages=['housecanary'],
+      packages=['housecanary', 'housecanary.excel'],
       install_requires=['requests','docopt', 'openpyxl'],
       zip_safe=False,
       test_suite='nose.collector',
