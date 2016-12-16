5ac4e8bd06450c1efc06d80bd66f15ead78e05c6
Jeff Francisco <jeff.francisco7@gmail.com>
2016-12-16 10:42:30 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='housecanary',
-      version='0.5.2',
+      version='0.6',
       description='Client Wrapper for the HouseCanary API',
       long_description=readme(),
       url='http://github.com/housecanary/hc-api-python',
@@ -13,7 +13,7 @@ setup(name='housecanary',
       author_email='techops@housecanary.com',
       license='MIT',
       packages=['housecanary', 'housecanary.excel'],
-      install_requires=['requests','docopt', 'openpyxl'],
+      install_requires=['requests', 'docopt', 'openpyxl'],
       zip_safe=False,
       test_suite='nose.collector',
       tests_require=['nose', 'mock'],
