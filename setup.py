b5a87edcf516f7f41d2a470da8fa32744f97e960
Chris Faulkner <thefaulkner@gmail.com>
2017-01-11 17:05:03 -0800

    Add client version to User-Agent.

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import codecs
+import os
+import re
+
 from setuptools import setup
 
 
-def readme():
-    with open('README.rst', 'rb') as f:
-        return f.read().decode('UTF-8')
+def read(*parts):
+    return codecs.open(os.path.join(*parts), 'r').read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(name='housecanary',
-      version='0.6.3',
+      version=find_version('housecanary', '__init__.py'),
       description='Client Wrapper for the HouseCanary API',
-      long_description=readme(),
+      long_description=read('README.rst'),
       url='http://github.com/housecanary/hc-api-python',
       author='HouseCanary',
       author_email='techops@housecanary.com',
@@ -19,8 +32,8 @@ setup(name='housecanary',
       test_suite='nose.collector',
       tests_require=['nose', 'mock'],
       entry_points={
-        'console_scripts': [
-            'hc_api_excel_concat=housecanary.hc_api_excel_concat.hc_api_excel_concat:main',
-            'hc_api_export=housecanary.hc_api_export.hc_api_export:main'
-        ]
+          'console_scripts': [
+              'hc_api_excel_concat=housecanary.hc_api_excel_concat.hc_api_excel_concat:main',
+              'hc_api_export=housecanary.hc_api_export.hc_api_export:main'
+          ]
       })
